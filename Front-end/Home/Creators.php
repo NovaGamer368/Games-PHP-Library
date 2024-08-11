4d652b33f01c2880e248f3530b2535be23df0ab4
Joey <48295680+Colbalt01@users.noreply.github.com>
2024-08-11 17:09:08 -0600

    Desc and Creators

--- Front-end/Home/Creators.php
+++ Front-end/Home/Creators.php
@@ -11,11 +11,11 @@ user-friendly, responsive, and functional. I am a great collaborator and effecti
 a part of a team of developers that work on projects that are challenging and will help me grow in my career. In my spare
 time, I enjoy playing Final Fantasy XIV and getting out to play ice hockey</p>
 
-<hr />
+<br />
 <h3>Joey Kaasch </h3>
 <p>I am a full stack developer. I like doing more of the backend, but I am able to do the front end if needed.</p>
 
-<hr />
+<br />
 <h3>Jonathan Brickwedde</h3>
     <p>firly motivated, and determined full stack developer. i love working with people to make applications that i hope user's enjoy.
     I like seeing how people can think of interesting ideas and seeing how they go about doing them. when im working on my own, 
--- Front-end/Home/WebsiteDescription.php
+++ Front-end/Home/WebsiteDescription.php
@@ -1,8 +1,7 @@
-<p>This website is going to be a video game library website using PHP as its primary code language. We will be displaying
-    the Name, Creator, Genre and the description of each of the games within a MySQL database. Along with this we will have a search
-    feature that will make going through the data a little easier. It there is no data returned we will be sending back the full list to avoid any issues.
-    There is also going to be a login option so you can login as an admin to manitpulate the data within the database from the website.
-    <br/>
-    There are also going to be some fun styles we are going to implement storing the preference of the user on their local storage. By
-    doing this we can keep the preference throughout testing and presentation.
-    </p>
+<p>
+    This website will be a video game library website using PHP as its primary code language. We will display the Name, Creator, Genre, and Description of each game within
+    a MySQL database. Along with this, we will have a search feature, which will make going through the data a little easier. If there is no data returned, we will be
+    sending back the full list to avoid any issues. There is going to be a login option, so you can log in as an admin to manipulate the data within the database from the
+    website. There are going to be some fun styles we are going to implement by storing the preferences of the user in their local storage. By doing this we can keep the
+    preference throughout testing and presentation.
+</p>
